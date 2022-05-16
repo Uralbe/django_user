280f258da8a51b29f59cda7bfdc1c0b79b7e42e8
Your Name <you@example.com>
2022-05-16 18:22:22 +0500

    fix

--- core/settings.py
+++ core/settings.py
@@ -6,6 +6,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import django_heroku
+django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
